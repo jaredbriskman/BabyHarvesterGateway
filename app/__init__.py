b215f357a197c3e7e5864563c411531f3fd6567e
jwb <jared.briskman@students.olin.edu>
2018-02-26 02:25:22 -0500

    Adds working basic HTTP Auth

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,45 @@
-from flask import Flask
+from flask import Flask, request, Response
 from flask_mqtt import Mqtt
+from functools import wraps
 
-app.config['MQTT_BROKER_URL'] = 'broker.hivemq.com'
+app = Flask(__name__)
+
+app.config['MQTT_BROKER_URL'] = ''
 app.config['MQTT_BROKER_PORT'] = 1883
 app.config['MQTT_USERNAME'] = ''
 app.config['MQTT_PASSWORD'] = ''
 app.config['MQTT_KEEPALIVE'] = 5
 app.config['MQTT_TLS_ENABLED'] = False
 
-app = Flask(__name__)
-mqtt = Mqtt(app)
+#mqtt = Mqtt(app)
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == 'secret'
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
 
 @app.route('/')
 def index():
     return 'Welcome to the Baby Harvester Gateway.'
+
+@app.route('/secret')
+@requires_auth
+def secret_page():
+    return "It's a secret."
